e269ac735d54bb758daf9017b6d9ec1a1cac187b
Helder Rodrigo <helderr_santosd@hotmail.com>
2022-12-10 20:29:38 -0300

    style: organization of routes and sources and personalized

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,46 @@
-import { registerRootComponent } from "expo";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useCallback } from 'react';
+
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+} from '@expo-google-fonts/poppins';
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { registerRootComponent } from 'expo';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native';
+
+import Routes from './routes';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
   });
 
-  registerRootComponent(App);
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <Routes />
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+registerRootComponent(App);
