efcc4795413408c0deab33c7ea165a47cd312cab
Helder Rodrigo <helderr_santosd@hotmail.com>
2022-12-10 20:26:48 -0300

    feat: global folder created with custom fonts and colors

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,25 @@
+export const theme = {
+  colors: {
+    black: '#1E1E1E',
+
+    gray200: '#202024',
+    gray100: '#29292E',
+
+    white200: '#E1E1E6',
+    white100: '#FFFFFF',
+
+    green300: '##00875F',
+    green200: '##00B37E',
+    green100: '##04D361',
+
+    purple: '#996DFF',
+  },
+
+  fonts: {
+    text400R: 'Roboto_400Regular',
+    text400: 'Poppins_400Regular',
+    text500: 'Poppins_500Medium,',
+    text600: 'Poppins_600SemiBold',
+    text700: 'Roboto_700Bold,',
+  },
+};
