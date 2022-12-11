38bd6a88d1e2861669b7ae571dd35251d6f14e4f
Helder Rodrigo <helderr_santosd@hotmail.com>
2022-12-11 15:27:17 -0300

    style: remove items for component

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,25 +1,58 @@
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  background-color: red;
+  background-color: ${({ theme }) => theme.colors.black};
   flex: 1;
-  justify-content: center;
-  align-items: center;
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 24px 16px;
+  align-items: flex-end;
+  background-color: ${({ theme }) => theme.colors.gray200};
+  height: 140px;
   width: 100%;
 `;
 
-export const TitleHeader = styled.Text`
-  font-size: 20px;
-  color: black;
+export const EventBox = styled.ScrollView``;
+
+export const FooterMain = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${({ theme }) => theme.colors.gray100};
+  height: 78px;
+  width: 100%;
 `;
 
-export const Button = styled.TouchableOpacity`
-  width: 200px;
-  height: 70px;
-  background-color: white;
-  margin: 20px;
-  color: black;
-  font-size: 10px;
+export const Footer = styled.TouchableOpacity`
+  flex-direction: row;
   align-items: center;
   justify-content: center;
+  margin: 24px;
+`;
+
+export const Ticket = styled.Image`
+  margin-right: 16px;
+`;
+
+export const FooterText = styled.Text`
+  color: ${({ theme }) => theme.colors.green100};
+  font-size: 14px;
+`;
+
+export const FooterContactor = styled.View`
+  margin-right: 24px;
+`;
+
+export const ContactorBox = styled.View`
+  padding: 2px 8px;
+  background-color: ${({ theme }) => theme.colors.gray300};
+  border-radius: 6px;
+`;
+
+export const Counter = styled.Text`
+  font-size: 14px;
+  color: white;
 `;
